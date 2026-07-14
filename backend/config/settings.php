d85f35b33c52dea371b2d03d3bb3bc0fb00a1d90
Alexey <panfilov.a@roistat.com>
2026-07-14 15:50:19 +0300

    added custom config file added redis to docker

---
+++ backend/config/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'reminders' => [
+        'limit' => 3,
+        'min_minutes' => 15,
+    ],
+    'lock_key' => [
+        'create_reminder' => 'create_reminder_user_'
+    ],
+];
